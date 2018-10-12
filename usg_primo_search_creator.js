6a8be6d93a34ccd0d239a7fb1e04e53c9cb4d815
Ben R <ber52305@uga.edu>
2018-10-12 12:37:47 -0400

    Removed jQuery fo pure javascript. Formatted result code for readablity

--- usg_primo_search_creator.js
+++ usg_primo_search_creator.js
@@ -968,11 +968,11 @@ function createDeepSearchLink() {
 	if(isEmpty(criteria)) {
 		var d = new Date();
 		var n = d.getTime();
-		$('#result').val('<div>\r	<script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>\r		<form>\r			<label for="' + n + '">' + label + '</label><input type="text" id="' + n + '"><a class="search_link' + n + '"><button type="submit">Search</button></a>\r	<script type="text/javascript">\r	$("a.search_link' + n + '").on("click",function' + n + ');\r	function function'+ n + '(){\r	var target = "');
+		$('#result').val('<div>\r	<form>\r		<label for="' + n + '">' + label + '</label>\r		<input type="text" id="' + n + '">\r		<a class="search_link' + n + '">\r			<button onclick="function'+n+'()" type="submit">Search</button>\r		</a>\r		<script type="text/javascript">\r		function function'+ n + '(){\r			var target = "');
 		$('#result').val($('#result').val() + url + '"');
 		$('#result').val($('#result').val() + "+document" + "." + "getElementById" + "('" + n + "')" + "." + "value");
 		if(facet_query.length>0) $('#result').val($('#result').val() + '+"' + facet_query + '"');
-		$('#result').val($('#result').val() + ";\r	window.open(target,'_blank');\r	}" + "\r	</script" + ">\r		</form>\r</div>");		
+		$('#result').val($('#result').val() + ";\r			window.open(target,'_blank');\r		}" + "\r		</script" + ">\r	</form>\r</div>");		
 		$('#result2').html(" ");
 	} else {
 		url += encodeURIComponent(criteria);
